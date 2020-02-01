823bb6f2702c94f54f211f1226319b968d4440c7
jlrhe <jan@heikel.fi>
2020-02-01 14:32:27 +0200

    initial commit

---
+++ src/main/java/fi/janheikel/palvelinohjelmointi/boardgameRandomizer/BoardgameRandomizerApplication.java
@@ -0,0 +1,13 @@
+package fi.janheikel.palvelinohjelmointi.boardgameRandomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameRandomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgameRandomizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/janheikel/palvelinohjelmointi/boardgameRandomizer/BoardgameRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.janheikel.palvelinohjelmointi.boardgameRandomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameRandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
