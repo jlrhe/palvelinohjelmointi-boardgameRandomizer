6c2074fab1fcfd7d4876983f5a4315f543f6ba96
jlrhe <jan@heikel.fi>
2020-02-13 21:35:51 +0200

    removed obsolete imports, made table prettier and a little responsive

--- src/main/java/fi/janheikel/palvelinohjelmointi/boardgameRandomizer/web/GameController.java
+++ src/main/java/fi/janheikel/palvelinohjelmointi/boardgameRandomizer/web/GameController.java
@@ -1,18 +1,13 @@
 package fi.janheikel.palvelinohjelmointi.boardgameRandomizer.web;
 
-
-
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import fi.janheikel.palvelinohjelmointi.boardgameRandomizer.domain.Game;
 import fi.janheikel.palvelinohjelmointi.boardgameRandomizer.domain.GameRepository;
